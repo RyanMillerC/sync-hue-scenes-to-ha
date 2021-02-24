eec2c76cc27a33135e21dbbdbd23b32531c9f6c0
Ryan Miller <ryan@devopsmachine.com>
2021-02-23 22:58:15 -0500

    Collect rooms into dictionary
    Not all groups are Room type. This gathers only rooms into a
    dictionary.

--- sync_hue.py
+++ sync_hue.py
@@ -10,4 +10,14 @@ except:
     print('Set HUE_BRIDGE_HOST and HUE_BRIDGE_USERNAME environment variables!')
     sys.exit(1)
 
-print(bridge.get_light())
+api_groups = bridge.get_group()
+
+rooms = {}
+for group_id, group_attributes in api_groups.items():
+    if group_attributes['type'] == 'Room':
+        rooms[group_id] = {
+            'name': group_attributes['name'],
+            'lights': group_attributes['lights']
+        }
+
+print(rooms)
